b782883935fbe319fece43375e2e614be62c623a
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2020-06-19 11:22:25 -0300

    Initial version of mrcontador_server generated by JHipster-6.9.1

---
+++ src/main/java/br/com/mrcontador/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.mrcontador.config.audit;
---
+++ src/main/java/br/com/mrcontador/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package br.com.mrcontador.web.rest.vm;
