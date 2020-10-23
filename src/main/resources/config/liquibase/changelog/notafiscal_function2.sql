2ccc1953c031d0c8fd67aacb8dd15f60c649f403
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2020-10-23 15:05:23 -0300

    update notafiscal function

--- src/main/resources/config/liquibase/changelog/notafiscal_function2.sql
+++ src/main/resources/config/liquibase/changelog/notafiscal_function2.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ds_demo.processa_notafiscal("pNOT_CODIGO" bigint)
+CREATE OR REPLACE FUNCTION ${schema}.processa_notafiscal("pNOT_CODIGO" bigint)
  RETURNS numeric
  LANGUAGE plpgsql
 AS $function$
@@ -7,14 +7,18 @@ pNOT_CODIGO ALIAS for $1;
 REC   RECORD; 
 vRETORNO NUMERIC;
 vHISTORICOFINAL TEXT;
+vTAXA NUMERIC;
 BEGIN
   vRETORNO:= 0;
 FOR REC IN 
 SELECT NF.ID AS NOTAFISCAL_ID, NF.NOT_NUMERO AS vNUMERONOTA, 
-NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, I.ID AS INTELIGENT_ID, I.HISTORICOFINAL as vHISTORICOFINAL, I.TIPO_VALOR as vTIPOVALOR
-    FROM ds_demo.INTELIGENT I INNER JOIN
-    ds_demo.COMPROVANTE C ON I.COMPROVANTE_ID = C.ID
-    , ds_demo.NOTAFISCAL NF 
+NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, 
+I.ID AS INTELIGENT_ID, I.HISTORICOFINAL as vHISTORICOFINAL, I.TIPO_VALOR as vTIPOVALOR, I.datalancamento as vDATAEXTRATO, I.numerodocumento as vNUMERODOCUMENTO,
+I.numerocontrole as vNUMEROCONTROLE, I.periodo as vPERIODO, I.cnpj as vCNPJ, I.beneficiario as vBENEFICIARIO, I.parceiro_id as vPARCEIROID, I.agenciabancaria_id as vAGENCIABANCARIAID, I.extrato_id as vCODIGEXTRATO,
+C.com_valorpagamento as vVALORPAGAMENTO, C.com_juros as vCOMJUROS, C.com_multa as vCOMMULTA, C.com_desconto AS vDESCONTO, C.id as vCOMPROVANTE
+    FROM ${schema}.INTELIGENT I INNER JOIN
+    ${schema}.COMPROVANTE C ON I.COMPROVANTE_ID = C.ID
+    , ${schema}.NOTAFISCAL NF 
     WHERE I.ASSOCIADO = false
     and (C.COM_CNPJ = NF.NOT_CNPJ or substring(C.COM_CNPJ,1,8) = substring(NF.NOT_CNPJ,1,8))
     and NF.NOT_VALORPARCELA+10 >= C.COM_VALORDOCUMENTO
@@ -25,19 +29,31 @@ NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, I.ID AS INTELIGEN
     AND NF.ID = pNOT_CODIGO
 
  LOOP
+ 	vTAXA:= REC.vVALORPAGAMENTO-REC.vPARCELANOTA-REC.vCOMJUROS-REC.vCOMMULTA+REC.vDESCONTO;
+ 	
  	IF (REC.vTIPOVALOR = 'PRINCIPAL') THEN
     vHISTORICOFINAL   := 'Pagto. NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
     ELSE
     vHISTORICOFINAL := REC.vHISTORICOFINAL || ' NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
     END IF;
-   	update ds_demo.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID where id = REC.INTELIGENT_ID;   	
-	vRETORNO:= vRETORNO + 1;
+    
+    IF (vTAXA > 0) THEN
+   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID, tipo_inteligent ='C'  where id = REC.INTELIGENT_ID;
+   	INSERT INTO  ${schema}.INTELIGENT (historico, historicofinal,tipo_valor,datalancamento,numerodocumento,numerocontrole,periodo,debito,associado,
+     		cnpj,beneficiario,tipo_inteligent,comprovante_id,parceiro_id,agenciabancaria_id, extrato_id) VALUES ('Pagto. de Taxa bancária','Pagto. de Taxa bancária','TAXA',REC.vDATAEXTRATO,REC.vNUMERODOCUMENTO,REC.vNUMEROCONTROLE,REC.vPERIODO, VTAXA,false,
+     		REC.vCNPJ,REC.vBENEFICIARIO, 'C',REC.vCOMPROVANTE,REC.vPARCEIROID,REC.vAGENCIABANCARIAID, REC.vCODIGEXTRATO);
+   	ELSE
+   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID  where id = REC.INTELIGENT_ID;
+   	END IF;
+	 SELECT ${schema}.PROCESSA_CONTA(REC.INTELIGENT_ID);
+   	vRETORNO:= vRETORNO + 1;
  END LOOP; 
 	if (vRETORNO > 0) THEN
-	update ds_demo.notafiscal set processado = true where ID = pNOT_CODIGO;
+	update ${schema}.notafiscal set processado = true where ID = pNOT_CODIGO;
 	else
-	select from ds_demo.processa_notafiscal_semdata(pNOT_CODIGO) into vRETORNO;
+	select from ${schema}.processa_notafiscal_semdata(pNOT_CODIGO) into vRETORNO;
 	end if;
+	
 RETURN COALESCE(vRETORNO ,0);
 END;
 $function$
--- src/main/resources/config/liquibase/changelog/processa_notafiscal_semdata.sql
+++ src/main/resources/config/liquibase/changelog/processa_notafiscal_semdata.sql
@@ -7,11 +7,16 @@ pNOT_CODIGO ALIAS for $1;
 REC   RECORD; 
 vRETORNO NUMERIC;
 vHISTORICOFINAL TEXT;
+vTAXA NUMERIC;
+
 BEGIN
   vRETORNO:= 0;
 FOR REC IN 
 SELECT NF.ID AS NOTAFISCAL_ID, NF.NOT_NUMERO AS vNUMERONOTA, 
-NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, I.ID AS INTELIGENT_ID, I.HISTORICOFINAL as vHISTORICOFINAL, I.TIPO_VALOR as vTIPOVALOR
+NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, 
+I.ID AS INTELIGENT_ID, I.HISTORICOFINAL as vHISTORICOFINAL, I.TIPO_VALOR as vTIPOVALOR, I.datalancamento as vDATAEXTRATO, I.numerodocumento as vNUMERODOCUMENTO,
+I.numerocontrole as vNUMEROCONTROLE, I.periodo as vPERIODO, I.cnpj as vCNPJ, I.beneficiario as vBENEFICIARIO, I.parceiro_id as vPARCEIROID, I.agenciabancaria_id as vAGENCIABANCARIAID, I.extrato_id as vCODIGEXTRATO,
+C.com_valorpagamento as vVALORPAGAMENTO, C.com_juros as vCOMJUROS, C.com_multa as vCOMMULTA, C.com_desconto AS vDESCONTO
     FROM ds_demo.INTELIGENT I INNER JOIN
     ds_demo.COMPROVANTE C ON I.COMPROVANTE_ID = C.ID
     , ds_demo.NOTAFISCAL NF 
@@ -24,12 +29,23 @@ NF.NOT_PARCELA AS vPARCELANOTA,NF.NOT_EMPRESA AS vEMPRESANOTA, I.ID AS INTELIGEN
     AND NF.ID = pNOT_CODIGO
 
  LOOP
+    vTAXA:= vVALORPAGAMENTO-vPARCELANOTA-vCOMJUROS-vCOMMULTA+vDESCONTO;
  	IF (REC.vTIPOVALOR = 'PRINCIPAL') THEN
-    vHISTORICOFINAL   := 'Pagto. NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
+	    vHISTORICOFINAL   := 'Pagto. NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
     ELSE
-    vHISTORICOFINAL := REC.vHISTORICOFINAL || ' NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
+	    vHISTORICOFINAL := REC.vHISTORICOFINAL || ' NFe '|| REC.vNUMERONOTA || '/' || REC.vPARCELANOTA || ' de ' || REC.vEMPRESANOTA;
     END IF;
-   	update ds_demo.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID where id = REC.INTELIGENT_ID;   	
+    
+    IF (vTAXA > 0) THEN
+	   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID, tipo_inteligent ='C'  where id = REC.INTELIGENT_ID;
+	   	INSERT INTO  ${schema}.INTELIGENT (historico, historicofinal,tipo_valor,datalancamento,numerodocumento,numerocontrole,periodo,debito,associado,
+	     		cnpj,beneficiario,tipo_inteligent,comprovante_id,parceiro_id,agenciabancaria_id, extrato_id) VALUES ('Pagto. de Taxa bancária','Pagto. de Taxa bancária','TAXA',vDATAEXTRATO,vNUMERODOCUMENTO,vNUMEROCONTROLE,vPERIODO, VTAXA,false,
+	     		vCNPJ,vBENEFICIARIO, 'C',vCOMPROVANTE,vPARCEIROID,vAGENCIABANCARIAID, vCODIGEXTRATO);
+   	ELSE
+	   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = REC.NOTAFISCAL_ID  where id = REC.INTELIGENT_ID;
+   	END IF;
+   	
+   	SELECT ${schema}.PROCESSA_CONTA(REC.INTELIGENT_ID);
 	vRETORNO:= vRETORNO + 1;
  END LOOP; 
 	if (vRETORNO > 0) THEN
