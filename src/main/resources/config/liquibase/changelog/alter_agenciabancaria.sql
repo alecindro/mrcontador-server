c44157e788cd483b58859d1bf74eeaeabd6666e7
alecindro <alecindrocastilho@gmail.com>
2020-10-26 23:37:58 -0300

    corrigido sql

--- src/main/resources/config/liquibase/changelog/alter_agenciabancaria.sql
+++ src/main/resources/config/liquibase/changelog/alter_agenciabancaria.sql
@@ -6,5 +6,5 @@
 ALTER TABLE ${schema}.agenciabancaria ALTER COLUMN ban_codigobancario DROP NOT NULL;
 ALTER TABLE ${schema}.agenciabancaria ALTER COLUMN conta_id DROP NOT NULL;
 ALTER TABLE ${schema}.agenciabancaria ALTER COLUMN conta_id DROP NOT NULL;
-ALTER TABLE ${schema}.agenciabancaria ADD COLUMN tipo_agencia varchar(255) NOT NULL;
+ALTER TABLE ${schema}.agenciabancaria ADD COLUMN tipo_agencia varchar(255) NULL;
 
