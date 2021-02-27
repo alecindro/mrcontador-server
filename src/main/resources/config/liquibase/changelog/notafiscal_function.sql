70cce06beffe192601ca5803a522c2c69ca3d889
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2021-02-27 12:47:08 -0300

    corrigido funcoes

--- src/main/resources/config/liquibase/changelog/notafiscal_function.sql
+++ src/main/resources/config/liquibase/changelog/notafiscal_function.sql
@@ -31,8 +31,9 @@ begin
 SELECT * 
 INTO selected_nota 
 from ${schema}.NOTAFISCAL NF WHERE NF.tno_codigo =0 AND NF.ID  = pNOT_CODIGO;
+
 SELECT i.id, i.datalancamento, i.numerodocumento, i.numerocontrole, i.periodo, i.cnpj,
-i.beneficiario, i.comprovante_id, i.parceiro_id, i.agenciabancaria_id, i.extrato_id, min(c.COM_VALORPAGAMENTO),  c.com_juros, c.com_multa, c.com_desconto 
+i.beneficiario, i.comprovante_id, i.parceiro_id, i.agenciabancaria_id, i.extrato_id, c.COM_VALORPAGAMENTO,  c.com_juros, c.com_multa, c.com_desconto 
 into vInteligent_id, vDatalancamento, vNumerodocumento, vNumerocontrole, vPeriodo, vCnpj, vBeneficiario, vComprovante_id,
 vParceiro_id, vAgenciabancaria_id, vExtrato_id, vPagamento, vCom_juros, vCom_multa, vCom_desconto
 from ${schema}.inteligent i
@@ -41,37 +42,58 @@ from ${schema}.inteligent i
     WHERE 
     i.tipo_valor = 'PRINCIPAL'
     and (i.cnpj = selected_nota.not_cnpj or substring(i.cnpj,1,8) = substring(selected_nota.not_cnpj,1,8))
-    and (c.TIPO_COMPROVANTE = 'TRANSFERENCIA' or c.TIPO_COMPROVANTE = 'TITULO')
-      and ((c.com_valordocumento = selected_nota.not_valorparcela AND c.COM_DATAVENCIMENTO >= cast((selected_nota.not_dataparcela - 7)as DATE)) OR
-    (c.com_valordocumento >= selected_nota.not_valorparcela AND c.COM_DATAVENCIMENTO = selected_nota.not_dataparcela))
+    and c.TIPO_COMPROVANTE = 'TITULO'
+      and (c.com_valordocumento >= selected_nota.not_valorparcela AND c.COM_DATAVENCIMENTO = selected_nota.not_dataparcela) 
 	AND i.PARCEIRO_ID = selected_nota.parceiro_id
     AND i.notafiscal_id IS NULL
-	group by c.id, i.id order by c.com_valordocumento asc limit 1;
+	order by c.com_valordocumento asc,c.com_datapagamento ASC limit 1;
+	
+if(vInteligent_id is null) then
+SELECT i.id, i.datalancamento, i.numerodocumento, i.numerocontrole, i.periodo, i.cnpj,
+i.beneficiario, i.comprovante_id, i.parceiro_id, i.agenciabancaria_id, i.extrato_id, c.COM_VALORPAGAMENTO,  c.com_juros, c.com_multa, c.com_desconto 
+into vInteligent_id, vDatalancamento, vNumerodocumento, vNumerocontrole, vPeriodo, vCnpj, vBeneficiario, vComprovante_id,
+vParceiro_id, vAgenciabancaria_id, vExtrato_id, vPagamento, vCom_juros, vCom_multa, vCom_desconto
+from ${schema}.inteligent i
+    inner join ${schema}.comprovante c 
+    on i.comprovante_id = c.id
+    WHERE 
+    i.tipo_valor = 'PRINCIPAL'
+    and (i.cnpj = selected_nota.not_cnpj or substring(i.cnpj,1,8) = substring(selected_nota.not_cnpj,1,8))
+    and c.TIPO_COMPROVANTE = 'TITULO'
+      and (c.com_valordocumento >= selected_nota.not_valorparcela AND c.COM_DATAVENCIMENTO >= selected_nota.not_dataparcela) 
+	AND i.PARCEIRO_ID = selected_nota.parceiro_id
+    AND i.notafiscal_id IS NULL
+	order by c.com_valordocumento asc,c.com_datapagamento ASC limit 1;
+end if;
+
+	if(vInteligent_id is null) then
+	SELECT i.id, i.datalancamento, i.numerodocumento, i.numerocontrole, i.periodo, i.cnpj,
+	i.beneficiario, i.comprovante_id, i.parceiro_id, i.agenciabancaria_id, i.extrato_id, c.COM_VALORPAGAMENTO,  c.com_juros, c.com_multa, c.com_desconto 
+	into vInteligent_id, vDatalancamento, vNumerodocumento, vNumerocontrole, vPeriodo, vCnpj, vBeneficiario, vComprovante_id,
+	vParceiro_id, vAgenciabancaria_id, vExtrato_id, vPagamento, vCom_juros, vCom_multa, vCom_desconto
+	from ${schema}.inteligent i
+    inner join ${schema}.comprovante c 
+    on i.comprovante_id = c.id
+    WHERE 
+    i.tipo_valor = 'PRINCIPAL'
+    and (i.cnpj = selected_nota.not_cnpj or substring(i.cnpj,1,8) = substring(selected_nota.not_cnpj,1,8))
+    and c.TIPO_COMPROVANTE = 'TRANSFERENCIA'
+      and (c.com_valordocumento >= selected_nota.not_valorparcela AND c.COM_DATAVENCIMENTO >= selected_nota.not_dataparcela) 
+	AND i.PARCEIRO_ID = selected_nota.parceiro_id
+    AND i.notafiscal_id IS NULL
+	order by c.com_valordocumento asc,c.com_datapagamento ASC limit 1;
+	end if;
  
 	if (vInteligent_id is not null) then
-    vTAXA:= vPagamento - selected_nota.not_valorparcela - vCom_juros - vCom_multa - vCom_desconto;
+    vTAXA:= vPagamento - selected_nota.not_valorparcela - vCom_juros - vCom_multa + vCom_desconto;
  	vHISTORICOFINAL   := 'Pagto. NFe '|| selected_nota.not_numero || '/' || selected_nota.not_parcela || ' de ' || selected_nota.NOT_EMPRESA;
     IF (vTAXA > 0) then
-   	   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = selected_nota.id, debito = (vPagamento-vTAXA) *-1, tipo_inteligent ='C'  where id = vInteligent_id;
+   	   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = selected_nota.id, debito = (vPagamento-vTAXA - vCom_juros - vCom_multa + vCom_desconto) *-1, tipo_inteligent ='C'  where id = vInteligent_id;
 	    vHISTORICOFINAL   := 'Pagto. de taxa bancária ref. '|| selected_nota.not_numero || '/' || selected_nota.not_parcela || ' de ' || selected_nota.NOT_EMPRESA;
-    	SELECT *
-	    FROM ${schema}.INTELIGENT 
-	    into selected_inteligent2
-	    WHERE tipo_valor = 'JUROS'
-	    and comprovante_id = vComprovante_id
-	    AND parceiro_id = selected_nota.parceiro_id
-		AND notafiscal_id IS NULL
-		ORDER BY datalancamento ASC
-		LIMIT 1;
-		
-			if (selected_inteligent2.id is not null) then
-			 update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = selected_nota.id, tipo_inteligent ='C'  where id = selected_inteligent2.id;
-			else
-				INSERT INTO  ${schema}.INTELIGENT (historico, tipo_valor,datalancamento,numerodocumento,numerocontrole,periodo,debito,associado,
+   			INSERT INTO  ${schema}.INTELIGENT (historico, tipo_valor,datalancamento,numerodocumento,numerocontrole,periodo,debito,associado,
 			     		cnpj,beneficiario,tipo_inteligent,comprovante_id,parceiro_id,agenciabancaria_id, extrato_id, notafiscal_id, historicofinal) VALUES 
 						('Pagto. de Taxa bancária','TAXA',vDatalancamento,vNumerodocumento,vNumerocontrole,vPeriodo, vTAXA*-1,false,
 			     		vCnpj,vBeneficiario, 'C',vComprovante_id,vParceiro_id,vAgenciabancaria_id,vExtrato_id, selected_nota.id, vHISTORICOFINAL);
-		   	END IF;
    	else
  	   	update ${schema}.INTELIGENT set historicofinal = vHISTORICOFINAL, notafiscal_id = selected_nota.id  where id = vInteligent_id;
    	END IF;
