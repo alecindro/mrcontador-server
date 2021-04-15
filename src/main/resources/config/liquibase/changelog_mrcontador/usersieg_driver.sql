0da5cc042b9820f5da9ba105a4642c650354a0eb
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2021-04-15 09:54:44 -0300

    adicionado usuarios integracao

---
+++ src/main/resources/config/liquibase/changelog_mrcontador/usersieg_driver.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql  
+--changeset mrcontador:12
+INSERT INTO ds_mrcontador.jhi_user (id,login,password_hash,first_name,last_name,email,image_url,activated,lang_key,activation_key,reset_key,funcao,datasource,created_by,created_date,reset_date,last_modified_by,last_modified_date) VALUES 
+(7,'integradorSeg','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','IntegradorSeg','Seg','integradorSeg@localhost.com',NULL,true,'pt-br',NULL,NULL,NULL,'ds_mrcontador','system',NULL,NULL,'system',NULL)
+;
+INSERT INTO ds_mrcontador.jhi_user (id,login,password_hash,first_name,last_name,email,image_url,activated,lang_key,activation_key,reset_key,funcao,datasource,created_by,created_date,reset_date,last_modified_by,last_modified_date) VALUES 
+(8,'integradorDriver','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','IntegradorDriver','Driver','integradorDriver@localhost.com',NULL,true,'pt-br',NULL,NULL,NULL,'ds_mrcontador','system',NULL,NULL,'system',NULL)
+;
+INSERT INTO ds_mrcontador.jhi_user_authority (user_id,authority_name) VALUES (7,'ds_mrcontador');
+INSERT INTO ds_mrcontador.jhi_user_authority (user_id,authority_name) VALUES (7,'ROLE_USER');
+INSERT INTO ds_mrcontador.jhi_user_authority (user_id,authority_name) VALUES (8,'ds_mrcontador');
+INSERT INTO ds_mrcontador.jhi_user_authority (user_id,authority_name) VALUES (8,'ROLE_USER');
